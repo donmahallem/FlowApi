d86f17f439da738f6cd0d7e91a309aeee3fb3262
DonMahallem <donmahallem@users.noreply.github.com>
2019-01-04 17:49:30 +0100

    JJ

--- src/flow-api.ts
+++ src/flow-api.ts
@@ -1,6 +1,5 @@
-import Axios, { AxiosResponse, AxiosPromise, AxiosError, AxiosTransformer } from "axios";
+
 import { Config } from "./config";
-import { CookieJar, Cookie } from "cookiejar";
 import * as cookieParser from "set-cookie-parser";
 import * as FormData from "form-data";
 import * as querystring from "querystring";
@@ -10,70 +9,12 @@ import { Agent } from "https";
 
 
 export class FlowApi {
-    private cookieJar: CookieJar = new CookieJar();
     private cookieJar2: request.CookieJar = request.jar();
     private readonly baseUrl: string = "https://flow.polar.com";
-    public getLogin(): AxiosPromise<any> {
-        return Axios.get(this.baseUrl + "/login", {
-            transformResponse: this.createCookieCatcher(this.cookieJar)
-        });
-    }
-
-    public getLoginHead(): AxiosPromise<any> {
-        return Axios.head(this.baseUrl + "/login", {
-            transformResponse: this.createCookieCatcher(this.cookieJar)
-        });
-    }
-
-    private createCookieInserter(jar: CookieJar): AxiosTransformer {
-        return (data: any, headers?: any): any => {
-            console.log("Inserter", data, headers);
-            return data;
-        }
-    }
-
-    private createCookieCatcher(jar: CookieJar): AxiosTransformer {
-        return (data: any, headers?: any): any => {
-            if (headers['set-cookie']) {
-                if (Array.isArray(headers['set-cookie'])) {
-                    for (let cookie of headers['set-cookie']) {
-                        this.cookieJar.setCookie(cookie, "flow.polar.com");
-                    }
-                }
-            }
-            return data;
-        };
-    }
 
 
-    public getJar(): ReadonlyArray<Cookie> {
-        return this.cookieJar.getCookies({ domain: "flow.polar.com", path: "/", secure: true, script: false });
-    }
     public getJar2(): request.CookieJar {
         return this.cookieJar2;
-    }
-    public signin() {
-
-        var data: any = {
-            email: Config.email,
-            password: Config.password,
-            returnUrl: "/"
-        };
-        console.log("Request data", querystring.stringify(data));
-        return Axios.post("https://flow.polar.com/login", querystring.stringify(data), {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Origin': 'https://flow.polar.com',
-                'Referer': 'https://flow.polar.com/login',
-                'Content-Length': querystring.stringify(data).length,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-            },
-            maxRedirects: 1,
-            transformRequest: this.createCookieInserter(this.cookieJar),
-            transformResponse: this.createCookieCatcher(this.cookieJar)
-        });
-
-
     }
     public signin3(): Promise<request.Response> {
         return new Promise((resolve, reject) => {
@@ -157,4 +98,6 @@ export class FlowApi {
         });
 
     }
+
+
 }
