3ab849d906f92435897cebb9a9fab576e926251e
donmahallem <donmahallem@users.noreply.github.com>
2019-01-09 11:19:56 +0100

    added more test cases for Merger add

--- src/summary-merger.spec.ts
+++ src/summary-merger.spec.ts
@@ -5,28 +5,59 @@ import "mocha";
 
 describe("SummaryMerger", () => {
     describe("add", () => {
-        let generateKeyStub: sinon.SinonStub;
-        let mergerInstance: merger.SummaryMerger;
-        beforeEach(() => {
-            mergerInstance = new merger.SummaryMerger(true);
-            generateKeyStub = sinon.stub(mergerInstance, "generateKey");
-        })
+        describe("-- error on duplicate in constructor", () => {
 
-        afterEach(() => {
-            generateKeyStub.restore();
-        });
+            let generateKeyStub: sinon.SinonStub;
+            let mergerInstance: merger.SummaryMerger;
+            beforeEach(() => {
+                mergerInstance = new merger.SummaryMerger(true);
+                generateKeyStub = sinon.stub(mergerInstance, "generateKey");
+            })
 
-        it("should throw error for duplicate", () => {
-            generateKeyStub.onCall(0).returns("asdf");
-            generateKeyStub.onCall(1).returns("asdf");
-            mergerInstance.add(<any>{});
-            expect(mergerInstance.add.bind(this, <any>{})).to.throw;
+            afterEach(() => {
+                generateKeyStub.restore();
+            });
+
+            it("should throw error for duplicate", () => {
+                generateKeyStub.onCall(0).returns("asdf");
+                generateKeyStub.onCall(1).returns("asdf");
+                mergerInstance.add(<any>{}, false);
+                expect(mergerInstance.add.bind(this, <any>{}, false)).to.throw;
+            });
+            it("should not throw error", () => {
+                generateKeyStub.onCall(0).returns("asdf");
+                generateKeyStub.onCall(1).returns("asdf");
+                mergerInstance.add(<any>{}, true);
+                expect(mergerInstance.add.bind(this, <any>{}, true)).to.not.throw;
+            });
+            //TODO: MORE TESTS FOR ADD WITH FORCE PARAM ETC
         });
-        it("should not throw error", () => {
-            generateKeyStub.onCall(0).returns("asdf");
-            generateKeyStub.onCall(1).returns("asdf2");
-            mergerInstance.add(<any>{});
-            expect(mergerInstance.add.bind(this, <any>{})).to.not.throw;
+        describe("-- no error on duplicate in constructor", () => {
+
+            let generateKeyStub: sinon.SinonStub;
+            let mergerInstance: merger.SummaryMerger;
+            beforeEach(() => {
+                mergerInstance = new merger.SummaryMerger(false);
+                generateKeyStub = sinon.stub(mergerInstance, "generateKey");
+            })
+
+            afterEach(() => {
+                generateKeyStub.restore();
+            });
+
+            it("should throw error for duplicate", () => {
+                generateKeyStub.onCall(0).returns("asdf");
+                generateKeyStub.onCall(1).returns("asdf");
+                mergerInstance.add(<any>{}, false);
+                expect(mergerInstance.add.bind(this, <any>{}, false)).to.not.throw;
+            });
+            it("should not throw error", () => {
+                generateKeyStub.onCall(0).returns("asdf");
+                generateKeyStub.onCall(1).returns("asdf");
+                mergerInstance.add(<any>{}, true);
+                expect(mergerInstance.add.bind(this, <any>{}, true)).to.not.throw;
+            });
+            //TODO: MORE TESTS FOR ADD WITH FORCE PARAM ETC
         });
     });
 });
