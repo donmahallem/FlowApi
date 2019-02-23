46dfeeacd216aac57c10f4351c78ccea1c4670e0
DonMahallem <donmahallem@users.noreply.github.com>
2019-02-23 14:54:34 +0100

    fix lint errors

--- src/flow-api-client.ts
+++ src/flow-api-client.ts
@@ -29,21 +29,21 @@ export class FlowApiClient {
      */
     public signin(mail: string, password: string): Promise<request.Response> {
         const data: any = {
-            returnUrl: "/",
             email: mail,
             password,
+            returnUrl: "/",
         };
         return new Promise((resolve, reject) => {
             request.post("https://flow.polar.com/login", {
                 form: data,
-                jar: this.cookieJar,
                 headers: {
                     // 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
                     // 'content-length': querystring.stringify(data).length,
                     "accept": "application/json",
                     "user-agent": this.userAgent,
                 },
-            }, function (err, httpResponse, body) {
+                jar: this.cookieJar,
+            }, (err, httpResponse, body) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -56,11 +56,11 @@ export class FlowApiClient {
     public getSleep(id: number): Promise<ISleepInterval[]> {
         return new Promise((resolve, reject) => {
             request.get("https://flow.polar.com/api/sleep/" + id, {
-                jar: this.cookieJar,
                 headers: {
                     "user-agent": this.userAgent,
                 },
-            }, function (err, httpResponse, body) {
+                jar: this.cookieJar,
+            }, (err, httpResponse, body) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -77,26 +77,30 @@ export class FlowApiClient {
         if (day < 1 || day > 31) {
             return Promise.reject(new Error("The day must be equal to or between 1 and 31"));
         }
-        const _year: string = "" + year;
-        const _month: string = (month < 10) ? ("0" + month) : ("" + month);
-        const _day: string = (day < 10) ? ("0" + day) : ("" + day);
+        const convYear: string = "" + year;
+        const convMonth: string = (month < 10) ? ("0" + month) : ("" + month);
+        const convDay: string = (day < 10) ? ("0" + day) : ("" + day);
         return new Promise((resolve, reject) => {
-            request.get("https://flow.polar.com/api/sleep/nights/nearby?date=" + _year + "-" + _month + "-" + _day, {
-                jar: this.cookieJar,
-                headers: {
-                    "user-agent": this.userAgent,
-                },
-            }, function (err, httpResponse, body) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(JSON.parse(body));
-                }
-            });
+            request.get("https://flow.polar.com/api/sleep/nights/nearby?date=" + convYear
+                + "-" + convMonth + "-" + convDay, {
+                    headers: {
+                        "user-agent": this.userAgent,
+                    },
+                    jar: this.cookieJar,
+                }, (err, httpResponse, body) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(JSON.parse(body));
+                    }
+                });
         });
     }
 
-    public getActivityTimelineForDay(year: number, month: number, day: number, sampleCount: number = 50000): Promise<IDaySummary> {
+    public getActivityTimelineForDay(year: number,
+                                     month: number,
+                                     day: number,
+                                     sampleCount: number = 50000): Promise<IDaySummary> {
         if (month < 1 || month > 12) {
             return Promise.reject(new Error("The month must be equal to or between 1 and 12"));
         }
@@ -106,16 +110,19 @@ export class FlowApiClient {
         if (sampleCount < 1) {
             return Promise.reject(new Error("Samplecount must be atleast 1"));
         }
-        const _year: string = "" + year;
-        const _month: string = (month < 10) ? ("0" + month) : ("" + month);
-        const _day: string = (day < 10) ? ("0" + day) : ("" + day);
+        const convYear: string = "" + year;
+        const convMonth: string = (month < 10) ? ("0" + month) : ("" + month);
+        const convDay: string = (day < 10) ? ("0" + day) : ("" + day);
         return new Promise((resolve, reject) => {
-            request.get("https://flow.polar.com/api/activity-timeline/load?day=" + _year + "-" + _month + "-" + _day + "&maxSampleCount=" + sampleCount, {
-                jar: this.cookieJar,
+            const url: string = "https://flow.polar.com/api/activity-timeline/load?day="
+                + convYear + "-" + convMonth + "-" + convDay
+                + "&maxSampleCount=" + sampleCount;
+            request.get(url, {
                 headers: {
                     "accept": "application/json",
                     "user-agent": this.userAgent,
                 },
+                jar: this.cookieJar,
             }, this.createResponseHandler(resolve, reject));
         });
     }
@@ -126,7 +133,7 @@ export class FlowApiClient {
                 reject(err);
                 return;
             }
-            if (httpResponse.statusCode == 200) {
+            if (httpResponse.statusCode === 200) {
                 const contentType: string = httpResponse.headers["content-type"];
                 if (contentType && contentType.startsWith("application/json")) {
                     resolve(JSON.parse(body));
