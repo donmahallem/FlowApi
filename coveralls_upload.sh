b1f2af61ce8cceb28f83f17173fac5faf80595bb
DonMahallem <donmahallem@users.noreply.github.com>
2019-03-16 13:53:19 +0100

    jj

--- coveralls_upload.sh
+++ coveralls_upload.sh
@@ -1,2 +1,5 @@
-npm install coveralls --no-save
-cat ./coverage/lcov.info | coveralls -v
+#!/bin/bash
+if [ "$TEST_SUITE" == "unit" ]; then
+    npm install coveralls --no-save
+    cat ./coverage/lcov.info | coveralls -v
+fi
