307262bd8549c7489da50a48fc20d9ec3d136e2a
DonMahallem <donmahallem@users.noreply.github.com>
2019-03-20 14:16:19 +0100

    more tests

--- src/flow-api-client.spec.ts
+++ src/flow-api-client.spec.ts
@@ -6,6 +6,7 @@ import * as request from 'request';
 import * as sinon from 'sinon';
 import { URL } from "url";
 import { beforeEach } from 'mocha';
+import { FlowDate } from '@donmahallem/flow-api-types';
 
 describe('flow-api-client', () => {
     describe('FlowApiClient', () => {
@@ -143,6 +144,69 @@ describe('flow-api-client', () => {
                     }, (err) => {
                         expect(err).to.equal(testValue);
                         expect(testRequest.callback).to.not.be.undefined;
+                    });
+                });
+            });
+        });
+        describe('getActivityTimelineForDay(date: FlowDate, sampleCount: number)', () => {
+            let createBaseUrlStub: sinon.SinonStub;
+            let getStub: sinon.SinonStub;
+            let sandbox: sinon.SinonSandbox;
+            let instance: flowApiClient.FlowApiClient;
+            const testDomain: string = 'http://test.domain';
+            before('init classes', () => {
+                sandbox = sinon.createSandbox();
+                instance = new flowApiClient.FlowApiClient();
+                createBaseUrlStub = sandbox.stub(instance, 'createBaseUrl');
+                getStub = sandbox.stub(instance, 'get');
+                createBaseUrlStub.returns(new URL(testDomain));
+            });
+            beforeEach('create test items', () => {
+            })
+            afterEach('reset stubs', () => {
+                sandbox.resetHistory();
+            });
+            after('restore all', () => {
+                sandbox.restore();
+            });
+            const testDates: FlowDate[] = [
+                new FlowDate(1234, 4, 12),
+                new FlowDate(2390, 9, 6),
+                new FlowDate(2512, 8, 15)
+            ]
+            describe('with default parameters', () => {
+                testDates.forEach((testDate) => {
+                    [2, 4, 8, 16].forEach((testSample) => {
+                        it('should finish successfully with "' + testDate + '" and "' + testSample + '"', () => {
+                            getStub.resolves(testDate);
+                            const testUrl: URL = new URL(testDomain);
+                            testUrl.pathname = "/api/activity-timeline/load";
+                            testUrl.searchParams.set("day", testDate.toString());
+                            testUrl.searchParams.set("maxSampleCount", "" + testSample);
+                            return instance.getActivityTimelineForDay(testDate, testSample)
+                                .then((value) => {
+                                    expect(value).to.equal(testDate);
+                                    expect(getStub.callCount).to.equal(1);
+                                    expect(getStub.getCall(0).args).to.deep.equal([testUrl]);
+                                });
+                        });
+                    });
+                });
+            });
+            describe('without default parameters', () => {
+                testDates.forEach((testDate) => {
+                    it('should finish successfully with "' + testDate + '"', () => {
+                        getStub.resolves(testDate);
+                        const testUrl: URL = new URL(testDomain);
+                        testUrl.pathname = "/api/activity-timeline/load";
+                        testUrl.searchParams.set("day", testDate.toString());
+                        testUrl.searchParams.set("maxSampleCount", "50000");
+                        return instance.getActivityTimelineForDay(testDate)
+                            .then((value) => {
+                                expect(value).to.equal(testDate);
+                                expect(getStub.callCount).to.equal(1);
+                                expect(getStub.getCall(0).args).to.deep.equal([testUrl]);
+                            });
                     });
                 });
             });
